6cf723b8de8a1464a4f0b828515980647a6425ca
beac0n <>
2021-02-16 22:45:01 +0100

    refactor

--- src/main/main.go
+++ src/main/main.go
@@ -8,14 +8,14 @@ import (
 )
 
 func main() {
-	keyBase64 := flag.String("key", "", "base64 encoded key")
+	keyBase64 := flag.String("key", "", "base64 encoded aes key")
 
 	// client flags
 	doGenKey := flag.Bool("gen-key", false, "generate base64 encoded aes key")
 	address := flag.String("address", "", "udp address")
 
 	// isServer flags
-	isServer := flag.Bool("server", false, "run in isServer mode")
+	isServer := flag.Bool("server", false, "run in server mode")
 	port := flag.String("port", "8080", "udp port")
 	timeFrame := flag.Int64("timeframe", int64(5), "timestamp in request must not be older than this timeframe (in seconds)")
 
